aa821e8fd05b58d20aa7716fe9053e7e511f7c23
Jason Yu <me@ycmjason.com>
2018-11-16 00:49:13 +0000

    better late santa message

--- src/views/Game/GameEnded.vue
+++ src/views/Game/GameEnded.vue
@@ -1,9 +1,33 @@
-<template>
-  <Card>
-    <div>
+<template functional>
+  <main>
+    <Card class="late_card">
+      <h3>Late Santa</h3>
       <p>
-        This game's secret santas have been allocated. Please contact your inviter.
+        This game's secret santas have been allocated. Join earlier next time mate.
       </p>
-    </div>
-  </Card>
+    </Card>
+    <p>Here is a little video for you to enjoy:</p>
+    <Card class="late_santa">
+      <img src="https://media.giphy.com/media/3o6ZtnGrjdCl0VoCCA/giphy.gif">
+    </Card>
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.late_card {
+  max-width: 470px;
+  margin-bottom: 1rem;
+}
+
+.late_santa {
+  max-width: 270px;
+  max-height: 480px;
+  box-sizing: content-box;
+}
+</style>
