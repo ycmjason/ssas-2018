165dada4119f69d9665b4dc6ff5ffb6a47d6aa70
Jason Yu <me@ycmjason.com>
2018-11-16 01:32:41 +0000

    removed console log in firebase/index

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,18 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
+firebase.initializeApp({
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: `${process.env.VUE_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
   databaseURL: `https://${process.env.VUE_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
   storageBucket: `${process.envFIREBASE_PROJECT_ID}.appspot.com`,
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-};
-
-console.log(process.env);
-console.log(config);
-
-firebase.initializeApp(config);
+});
 
 export default firebase;
